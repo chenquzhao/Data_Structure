7760cd511f9b5004a905daee883c75cf1f5965bb
Chenqu Zhao <37530543+chenquzhao@users.noreply.github.com>
2020-08-30 18:52:34 +0800

    Update 2_double_linked_list.py

--- ch01 Basic Data Structure/1.1 Linked List/2_double_linked_list.py
+++ ch01 Basic Data Structure/1.1 Linked List/2_double_linked_list.py
@@ -1,1 +1,13 @@
+class DListNode:
+    def __init__(self, val):
+        self.val = val
+        self.prev = self.next = None
 
+    def reverse(self, head):
+        newHead = None
+        while head:
+            newHead = head
+            head = head.next
+            newHead.next = newHead.prev
+            newHead.prev = head
+        return newHead
