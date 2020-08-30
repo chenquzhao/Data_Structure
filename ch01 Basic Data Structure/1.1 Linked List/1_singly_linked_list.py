8fa6940a757cc2c809347c377b2b5f15fe816e37
Chenqu Zhao <37530543+chenquzhao@users.noreply.github.com>
2020-08-30 17:44:52 +0800

    Update 1_singly_linked_list.py

--- ch01 Basic Data Structure/1.1 Linked List/1_singly_linked_list.py
+++ ch01 Basic Data Structure/1.1 Linked List/1_singly_linked_list.py
@@ -3,15 +3,14 @@ class ListNode:
         self.val = val
         self.next = None
 
+    def reverse(self, head):
+        newHead = None
+        while head:
+            # head delete for old linked list
+            node = head
+            head = head.next
 
-node1 = ListNode(1)
-node2 = ListNode(2)
-node3 = ListNode(3)
-
-node1.next = node2
-node2.next = node3
-
-prob = node1
-while prob is not None:
-    print(prob.val)
-    prob = prob.next
+            # head insert for new linked list
+            node.next = newHead
+            newHead = node
+        return newHead
