df08d3bcbe02af43f582f5fe8a17782ac4878645
Chenqu Zhao <37530543+chenquzhao@users.noreply.github.com>
2020-08-31 22:41:05 +0800

    Create 1_stack.py

---
+++ ch01 Basic Data Structure/1.5 Stack/1_stack.py
@@ -0,0 +1,9 @@
+stack = []
+len(stack)  # size of stack
+stack[-1]  # get the top element
+stack.append()  # insert element to the top
+stack.pop()  # delete the top element
+
+# more efficient stack
+import collections
+stack = collections.deque()
