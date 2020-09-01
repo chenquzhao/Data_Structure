dc3752423e6728eb12ddfe8c79403431d15f319d
Chenqu Zhao <37530543+chenquzhao@users.noreply.github.com>
2020-09-01 22:02:29 +0800

    Create 3_insertion_sort.py

---
+++ ch02 Basic Sorting/3_insertion_sort.py
@@ -0,0 +1,14 @@
+def insertion_sort(alist):
+    for i, item_i in enumerate(alist):
+        print(alist)
+        index = i
+        while index > 0 and alist[index - 1] > item_i:
+            alist[index] = alist[index - 1]
+            index -= 1
+
+        alist[index] = item_i
+    return alist
+
+
+unsorted_list = [6, 5, 3, 1, 8, 7, 2, 4]
+print(insertion_sort(unsorted_list))
